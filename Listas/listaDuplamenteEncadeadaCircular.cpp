4436b67a39ecf6a1c0aca9bd0c5127d7ce97bfad
AntonioLaureno28 <antoniopedro2802@gmail.com>
2024-08-07 12:57:34 -0300

    A

--- Listas/listaDuplamenteEncadeadaCircular.cpp
+++ Listas/listaDuplamenteEncadeadaCircular.cpp
@@ -22,8 +22,23 @@ public:
     }
 
     void inserir(int x){
-        
+        node* aux = new node();
+        aux->valor = x;
+        if (sentinela == nullptr){ //se estiver vazio;
+            aux->prox = aux;
+            aux->ant = aux;
+            sentinela = aux;
+        } else {
+            aux->prox = sentinela->prox;
+            aux->ant = sentinela;
+            sentinela->prox->ant = aux;
+            sentinela->prox = aux;
+        }
+
+        cout << "Elemento inserido com sucesso!";
     }
+
+    
 
 
 
